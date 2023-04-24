a02dd226c31a7b2c42bc04f786bb7d0392cd9239
liulka_s <asl.warmhaus@gmail.com>
2023-04-24 12:13:06 +0200

    Update task-07.js

--- js/task-07.js
+++ js/task-07.js
@@ -4,7 +4,7 @@ const spanTextEl = document.querySelector('#text');
 spanTextEl.style.fontSize = `${inputEl.value}px`;
 
 function handleleFontSizeTextInput () {
-   return spanTextEl.style.fontSize = `${inputEl.value}px`
+    return spanTextEl.style.fontSize = `${inputEl.value}px`;
 };
 
 inputEl.addEventListener('input', handleleFontSizeTextInput);
